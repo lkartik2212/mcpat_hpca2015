2af605c01ad7f7e1986505eb1f277f94ec40c366
lkartik2212 <kartik.cciprep@gmail.com>
2025-03-07 06:58:33 +0100

    Update sweep_l1d.sh

--- scripts/sweep_l1d.sh
+++ scripts/sweep_l1d.sh
@@ -5,8 +5,8 @@ mkdir -p l1d_sweep
 for size in 32 64 128 256;do
     l1d_size=$((size*1024))
     #echo "$l1d_size"
-    #sed -i "s/\(<param name=\"dcache_config\" value=\"\)[0-9]\+/\1$l1d_size/" ../models/MR2.xml
-    #../mr2/mcpat -infile ../models/MR2.xml -print_level 5 &> l1d_sweep/area_l1d_${size}KB.txt
+    sed -i "s/\(<param name=\"dcache_config\" value=\"\)[0-9]\+/\1$l1d_size/" ../models/MR2.xml
+    ../mr2/mcpat -infile ../models/MR2.xml -print_level 5 &> l1d_sweep/area_l1d_${size}KB.txt
     area_value=$(awk 'NR==211 {print $3}' l1d_sweep/area_l1d_${size}KB.txt)
     echo "L1-D $size KB  -->  $area_value mm^2"
 done
