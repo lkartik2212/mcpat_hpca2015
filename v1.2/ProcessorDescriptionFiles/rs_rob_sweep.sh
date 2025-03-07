0ddba3c2634f433d198dd31383ddf20b594e9253
Kartik Lakshminarasimhan <kartik.cciprep@gmail.com>
2025-03-07 02:19:58 +0100

    [HPCA 2015] Source code from Xi, S. L., Jacobson, H., Bose, P., Wei, G. Y., & Brooks, D. (2015, February). Quantifying sources of error in McPAT and potential impacts on architectural studies. In 2015 IEEE 21st International symposium on high performance computer architecture (HPCA) (pp. 577-589). IEEE.

---
+++ v1.2/ProcessorDescriptionFiles/rs_rob_sweep.sh
@@ -0,0 +1,4 @@
+
+../mcpat -infile ARM_A9_2GHz_rs_8.xml
+
+sed -i '63s/^.*$/mystring/' ARM_A9_2GHz_rs_8.xml
---
+++ v1.2/ProcessorDescriptionFiles/sample.sh
@@ -0,0 +1,2 @@
+../mcpat -infile Alpha21364.xml &> alpha_36.txt
+../mcpat -infile Alpha21364_2.xml &> alpha_8.txt
